09173fc21de54a392cfe984c3db28c85aecd1c04
i-ashy <ashleymelinares@gmail.com>
2023-04-10 18:30:33 -0600

    mod header

--- Vista/Layout/Header.php
+++ Vista/Layout/Header.php
@@ -6,15 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../Layout/css/header.css">
     <title>La Cuponera</title>
 </head>
 <body>
-    <header class="bg-danger">
-        <nav class="navbar navbar-expand-sm fixed-top navbar-light bg-danger" style="padding: 10px;">
-            <a class="navbar-brand" style="color:#FFFFFF; font-weight: bold;" href="../Cupones/Inicio.php">
-            <img src="../Layout/img/cupon.png" width="30" height="30" class="d-inline-block align-top" alt="logo">
-            LA CUPONERA
-            </a>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+        <div class="container-fluid">
+            <a class="navbar-brand ms-5" style="color:#FFFFFF; font-weight: bold;" href="../Cupones/Inicio.php">
+            <img src="../Layout/img/cupon.png" alt="LOGO" width="30" height="24" class="d-inline-block align-text-top">    
+            LA CUPONERA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav nav-pills justify-content-end">
+                <li class="nav-item">
+                <a class="nav-link ms-5 active" data-bs-toggle="pill" style="color:#FFFFFF;" href="../Cupones/Inicio.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="pill" style="color:#FFFFFF;" href="">Belleza</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="pill" style="color:#FFFFFF;" href="">Lugares</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="pill" style="color:#FFFFFF;" href="">Servicios</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <!--
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -26,5 +49,6 @@
                     </ul>
                 </div>
         </nav>
+-->
     </header>
 </body>
